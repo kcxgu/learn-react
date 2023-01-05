c9f2231a3360d7fd13ea1af00f3ef550c6b27b86
Kaycee Gu <kayceegu@Kaycees-MacBook-Air.local>
2023-01-05 14:40:44 +0000

    completed stopwatch

--- 01-stopwatch/src/App.js
+++ 01-stopwatch/src/App.js
@@ -18,23 +18,39 @@ function App() {
   }, [running]);
 
   return (
-    <>
-      <h1>01-Stopwatch</h1>
-      <div>
+    <div className='flex flex-col items-center justify-center py-8'>
+      <h1 className='text-2xl font-semibold'>01-Stopwatch</h1>
+      <div className='text-xl font-semibold py-4'>
         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
         <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
       </div>
-      <div>
+      <div className='w-1/3 max-w-sm flex flex-row justify-evenly'>
         {running ? (
-          <button onClick={() => { setRunning(false) }}>Stop</button>
+          <button
+            className='border rounded-lg py-1 px-3.5'
+            onClick={() => { setRunning(false) }}
+          >
+            Stop
+          </button>
         ) : (
-          <button onClick={() => { setRunning(true) }}>Start</button>
+          <button
+            className='border rounded-lg py-1 px-3'
+            onClick={() => { setRunning(true) }}
+          >
+            Start
+          </button>
         )
         }
-        <button onClick={() => { setTime(0) }}>Reset</button>
+        <button
+          className='border rounded-lg py-1 px-2.5'
+          onClick={() => { setTime(0) }
+          }
+        >
+          Reset
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# learn-react
+# Learn React Through Mini Projects
+
+## 01. Stopwatch or the Count-Up Timer
+
+Key Skills Learned:
+- useState
+- useEffect
+- Conditional Rendering
+- Calculating milliseconds, seconds, minutes
