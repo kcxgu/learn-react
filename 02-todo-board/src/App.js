be50257cecf66ced141e26d5d1312761434fcdf5
Kaycee Gu <kayceegu@Kaycees-MacBook-Air.local>
2023-01-06 19:38:43 +0000

    complete input

--- 02-todo-board/src/App.js
+++ 02-todo-board/src/App.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import Input from "./components/Input";
+
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
   return (
-    <>
-      <h1>02 - To Do Board</h1>
-    </>
+    <div className="flex flex-col items-center justify-center py-8 gap-4">
+      <h1 className="text-xl font-semibold">02 - To Do Board</h1>
+      <Input taskList={taskList} setTaskList={setTaskList} />
+      <div>
+        {taskList.map((task, index) =>
+          <li key={index}>{task}</li>
+        )}
+      </div>
+    </div>
   );
 }
 
---
+++ 02-todo-board/src/components/Input.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+const Input = ({ taskList, setTaskList }) => {
+    const [input, setInput] = useState("")
+
+    const handleAddTask = (e) => {
+        e.preventDefault();
+        setTaskList([...taskList, input]);
+        setInput("");
+    }
+
+    return (
+        <>
+            <form className="flex flex-row items-center gap-3">
+                <input
+                    className="border rounded-lg py-1.5 px-2.5 text-lg"
+                    type="text"
+                    placeholder="Add a task"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button
+                    className="bg-violet-400 text-white py-2 px-3.5 rounded-lg"
+                    onClick={handleAddTask}
+                >
+                    Add
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default Input
