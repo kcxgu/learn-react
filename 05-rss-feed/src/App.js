92da6888184b5946983246a1dc3346d9a2087f21
Kaycee Gu <kayceegu@Kaycees-MacBook-Air.local>
2023-01-21 17:38:12 +0000

    completed css

--- 05-rss-feed/src/App.js
+++ 05-rss-feed/src/App.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Feed from "./Feed";
 
 function App() {
   const [articles, setArticles] = useState([]);
@@ -21,8 +22,21 @@ function App() {
 
   return (
     <>
-      <h1>05 - RSS Feed</h1>
-
+      <h1 className="text-xl font-semibold text-center mt-5">05 - RSS Feed</h1>
+      <h2 className="text-3xl font-semibold text-center mt-2 mb-4">Good morning, Kaycee</h2>
+      <div className="w-3/4 max-w-lg border mx-auto p-5 rounded-xl">
+        <div className="bg-black flex flex-col items-center rounded-lg py-3 px-6 mb-5">
+          <img src="https://cdn-images-1.medium.com/max/606/1*rOPLUJ3W6FUA3rO1U1IeuA@2x.png" alt="netflix technology blog rss feed" />
+        </div>
+        {articles.map((item, i) =>
+          <Feed
+            key={i}
+            title={item.item.title}
+            link={item.item.link}
+            date={item.item.pubDate}
+          />
+        )}
+      </div>
     </>
   );
 }
---
+++ 05-rss-feed/src/Feed.jsx
@@ -0,0 +1,16 @@
+const Feed = ({ title, link, date }) => {
+
+    let formatted = { day: "numeric", month: "long", year: "numeric" }
+    let articleDate = new Date(date).toLocaleDateString("en-GB", formatted)
+
+    return (
+        <>
+            <a href={link} target="_blank" rel="noopener noreferrer" className="hover:opacity-70">
+                <h3 className="text-xl mb-1">{title}</h3>
+                <p>{articleDate}</p>
+            </a>
+        </>
+    )
+}
+
+export default Feed;
