53fcfb39c158926e8e6c95583c79f7a45f0006e8
Kaycee Gu <kayceegu@Kaycees-MacBook-Air.local>
2023-01-21 15:59:54 +0000

    completed get request

--- 05-rss-feed/src/App.js
+++ 05-rss-feed/src/App.js
@@ -1,4 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  console.log(articles);
+
+  const getArticles = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/");
+      setArticles(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getArticles();
+  }, [])
+
   return (
     <>
       <h1>05 - RSS Feed</h1>
