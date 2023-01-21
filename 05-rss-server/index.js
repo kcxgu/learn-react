e7af734f733feaa5392a5fa0abca36485965c059
Kaycee Gu <kayceegu@Kaycees-MacBook-Air.local>
2023-01-21 16:00:49 +0000

    completed foundational server

--- 05-rss-server/index.js
+++ 05-rss-server/index.js
@@ -1,11 +1,29 @@
 import cors from "cors";
 import express from "express";
+import RSSParser from "rss-parser";
+
+const feedURL = "https://netflixtechblog.com/feed"
+const parser = new RSSParser();
+let articles = [];
+
+const parse = async url => {
+    const feed = await parser.parseURL(url);
+
+    feed.items.forEach(item => {
+        articles.push({ item })
+    })
+}
+parse(feedURL);
 
 let app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send(articles);
+})
+
 const server = app.listen("4000", () => {
-    console.log(`App is listening at http://localhost:4000`);
-});
+    console.log("App is listening at http://localhost:4000");
+})
 
 export default server;
